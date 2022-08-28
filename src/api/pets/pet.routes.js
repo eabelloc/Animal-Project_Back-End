60892edf7e19d6b21a899fe7e5c28a2ab91c13ff
Elias Bello <eabelloc.dev@gmail.com>
2022-08-29 00:02:23 +0200

    Pequeño-cambio-a-Routes

--- src/api/pets/pet.routes.js
+++ src/api/pets/pet.routes.js
@@ -4,7 +4,7 @@ const upload = require("../../middlewares/file")
 
 const { getAllPets, register, login, petById, update, remove } = require("./pet.controller");
 
-PetRoutes.get('/', getAllPets);
+PetRoutes.get('/', [authorize], getAllPets);
 PetRoutes.post('/register', upload.single("avatar"), register);
 PetRoutes.post('/login', login);
 PetRoutes.get('/:id', [authorize], petById);
